979f86b6560691040ea522f7e7c5ce8ba3c34fa3
praiseopara <cpopara2010@yahoo.com>
2021-10-17 13:21:07 -0500

    login and signup done

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+// import { createUser } from '../utils/API';
+import {ADD_USER} from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,6 +12,8 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  // use mutation
+  const [addUser, {error}] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -27,16 +30,22 @@ const SignupForm = () => {
       event.stopPropagation();
     }
 
+    // use try/catch instead of promises to handle errors
     try {
-      const response = await createUser(userFormData);
+      // const response = await createUser(userFormData);
+      const {data} = await addUser({
+        variables: {...userFormData}
+      });
+      // console.log(data)
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      // const { token, user } = await response.json();
+      // console.log(user);
+      // Auth.login(token);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
